9efcf63d960851bf6cc87f9cd1d01d5f1116cdbc
Stephen Roller <roller@cs.utexas.edu>
2011-02-14 18:37:03 -0600

    Nicer repl output.

--- repl.py
+++ repl.py
@@ -3,6 +3,10 @@ import sys
 from solver import solutions as solutions_finder
 from parser import SATSyntaxError, SATEndOfFileError
 
+def nice_display(variables):
+    return "  ".join( (v and " %s" or "!%s") % k 
+                      for k,v in variables.iteritems() )
+
 if __name__ == '__main__':
     while True:
         try:
@@ -10,16 +14,17 @@ if __name__ == '__main__':
             if not F:
                 continue
             
-            # keep getting input until we have a full expression
             while True:
+                # keep getting input until we have a full expression
                 try:
                     solutions = solutions_finder(F)
-                    print "? " + F
                     break
                 except SATEndOfFileError:
+                    # not done getting the full expression
                     F += " " + raw_input('... ').strip()
                     continue
                 except SATSyntaxError, se:
+                    # syntax error! report it
                     print "! " + F 
                     sys.stdout.write('-' * (se.charnum + 2) + '^\n')
                     sys.stdout.write(se.mesg + '\n')
@@ -27,7 +32,7 @@ if __name__ == '__main__':
             
             satisfied = False
             for solution in solutions:
-                print "=>", solution
+                print "= ", nice_display(solution)
                 satisfied = True
             if not satisfied:
                 print "unsatisfiable."
